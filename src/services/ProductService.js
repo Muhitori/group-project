aa51972330c75646daebb55c9ad7184d553660c1
Olha Pavlenko <olgaigpavlenko@gmail.com>
2021-06-22 13:18:54 +0300

    corrected ProductService

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,17 +2,10 @@ import { HTTPService } from './HTTPService';
 import { API_URL } from '../utils/constants';
 
 export class ProductService {
-  constructor() {
-    this.baseUrl = API_URL;
-  }
-
   async getProducts({ limit, token }) {
-    const data = await HTTPService.get(
-      `${this.baseUrl}/products?_limit=${limit}`,
-      {
-        Authorization: `Bearer ${token}`,
-      }
-    );
+    const data = await HTTPService.get(`${API_URL}/products?_limit=${limit}`, {
+      Authorization: `Bearer ${token}`,
+    });
     return data;
   }
 }
