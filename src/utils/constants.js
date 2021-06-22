0b68547fc8def84f61db7a2d8aabeb0fd28723cc
turevych.m <turevych.mykola@nixsolutions.com>
2021-06-22 14:54:48 +0300

    slide content moved to constants, added package-lock

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,36 @@
+export const API_URL = process.env.REACT_APP_API_URL;
+
 export const MAIN_COLOR = '#fff';
 export const LIGHT_COLOR = '#e0e0e0';
 export const sliderArrowColor = '#d9d9d9';
-export const API_URL = process.env.REACT_APP_API_URL;
+
+export const slidesContent = [
+  {
+    slideNumber: 1,
+    imageUrl:
+      'https://langate.com/wp-content/uploads/2020/12/code-refactoring.jpg',
+  },
+  {
+    slideNumber: 2,
+    imageUrl:
+      'https://www.letstute.com/assets/img/course_banner/rhapsody_in_blue_by_verticaldubai-d4yxrhk-900x300.jpg',
+  },
+  {
+    slideNumber: 3,
+    imageUrl: 'https://images.alphacoders.com/119/thumb-1920-119481.jpg',
+  },
+  {
+    slideNumber: 4,
+    imageUrl:
+      'https://sm.mashable.com/mashable_in/photo/default/nasa-galaxy_9pu4.jpg',
+  },
+  {
+    slideNumber: 5,
+    imageUrl:
+      'https://i.pinimg.com/originals/88/bc/35/88bc35b5833487afaff407296b531fcb.jpg',
+  },
+  {
+    slideNumber: 6,
+    imageUrl: 'https://cdn.hipwallpaper.com/i/89/85/0G2Yy5.jpg',
+  },
+];
