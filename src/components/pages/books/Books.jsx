0bb2592b29908e49f66695dac11f8d8252979924
turevych.m <turevych.mykola@nixsolutions.com>
2021-06-23 19:30:25 +0300

    books init load fixed

--- src/components/pages/books/Books.jsx
+++ src/components/pages/books/Books.jsx
@@ -1,17 +1,15 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Pagination } from '../../pagination/Pagination';
 import { Carousel } from '../../common/carousel/Carousel';
 import { BooksList } from '../../shared/books/BooksList';
-import { tokenSelector } from '../../../store/selectors/auth-selector';
 import { getPageCount } from '../../../store/slices/product-slice';
 
 export const Books = () => {
-  const token = useSelector(tokenSelector);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPageCount(token));
+    dispatch(getPageCount());
   }, []);
 
   return (
