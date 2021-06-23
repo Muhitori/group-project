44ac49d1887168063b517d076053f23753e9cec4
Olya Harbuz <harbuz.olha@nixsolutions.com>
2021-06-23 13:19:14 +0300

    load page count on redirect to books

--- src/components/pages/books/Books.jsx
+++ src/components/pages/books/Books.jsx
@@ -1,8 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Pagination } from '../../pagination/Pagination';
 import { Carousel } from '../../common/carousel/Carousel';
 import { BooksList } from '../../shared/books/BooksList';
+import { tokenSelector } from '../../../store/selectors/auth-selector';
+import { getAllProducts } from '../../../store/slices/product-slice';
 
 export const Books = () => {
+  const token = useSelector(tokenSelector);
+  const dispatch = useDispatch();
+  dispatch(getAllProducts(token));
   return (
     <>
       <Carousel />
